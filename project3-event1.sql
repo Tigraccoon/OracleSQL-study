679c2368536a6693cc09e231bda085f8084704a9
Tigraccoon <dlwogh0801@naver.com>
2019-03-26 17:43:23 +0900

    20190326

--- project3-event1.sql
+++ project3-event1.sql
@@ -2,8 +2,8 @@ create table event(
 fileName varchar2(2000),     --파일명
 fileRealName varchar2(2000), --원본파일명
 name varchar2(500),          --이름
-phoneNum varchar2(500),      --핸드폰번호
-email varchar2(1000)         --이메일주소
+email varchar2(1000),         --이메일주소
+phoneNum varchar2(500)      --핸드폰번호
 );
 
 select * from event;
