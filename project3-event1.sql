b40f96b676bb9dc7c176a407c234b066968e47f9
Tigraccoon <dlwogh0801@naver.com>
2019-03-28 16:55:53 +0900

    Update project3-event1.sql

--- project3-event1.sql
+++ project3-event1.sql
@@ -1,8 +1,8 @@
 create table event(
-fileName varchar2(2000),     --파일명
+fileName varchar2(2000),       --파일명
 fileRealName varchar2(2000), --원본파일명
-name varchar2(500),          --이름
-email varchar2(1000),         --이메일주소
+name varchar2(500),             --이름
+email varchar2(1000),            --이메일주소
 phoneNum varchar2(500)      --핸드폰번호
 );
 
